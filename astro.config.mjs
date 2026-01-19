726406bc80878ed141220aec3aff86c11c35fc5e
dev-ahad-ali <ahadali.dev@gmail.com>
2026-01-19 13:47:04 +0600

    Update: preact package added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://vook-blogs.netlify.app",
+  site: "https://vook-blogs.netlify.app",
+  integrations: [preact()],
 });
